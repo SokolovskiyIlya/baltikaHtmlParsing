cf04081e7dec0024302d21694e8f28de0764a8af
Соколовский Илья Игоревич <sokolguitarman@gmail.com>
2024-03-27 23:14:49 +0300

    Ready Ozon parser

--- Solution1/BaltikaParser/Program.cs
+++ Solution1/BaltikaParser/Program.cs
@@ -6,7 +6,10 @@ namespace Parser
         static async Task Main(string[] args)
         {
             var parser = new WebParser();
-            var dict = await parser.ParseWb("https://www.wildberries.ru/catalog/24825388/detail.aspx");
+            var wbDict = await parser.ParseWb(
+                "https://www.wildberries.ru/catalog/24825388/detail.aspx");
+            var ozonDict = await parser.ParseOzon(
+                "https://www.ozon.ru/product/pivnoy-napitok-baltika-0-greypfrut-bezalkogolnoe-24-sht-h-0-33-l-banka-866130708/");
         }
     }
 }
--- Solution1/BaltikaParser/WebParser.cs
+++ Solution1/BaltikaParser/WebParser.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using AngleSharp;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V121.Fetch;
 using SeleniumUndetectedChromeDriver;
 namespace Parser
 {
@@ -66,33 +67,37 @@ namespace Parser
             _driver.GoToUrl(url);
             Dictionary<string, string> result = new Dictionary<string, string>();
             await Task.Delay(5000);
-
-            _driver.FindElement(By.XPath("//button[@class='product-page__btn-detail j-wba-card-item j-wba-card-item-show j-wba-card-item-observe']")).Click();
-            await Task.Delay(5000);
-
             var html = _driver.PageSource;
             var config = Configuration.Default;
             var context = BrowsingContext.New(config);
             var doc = await context.OpenAsync(req => req.Content(html));
-
-            var descriptionElements = doc.Body.QuerySelectorAll("div.product-details .product-params__row");
+            var descriptionElements = doc.Body.QuerySelectorAll("#section-characteristics dl");
             foreach (var element in descriptionElements)
             {
-                string key = element.QuerySelector("th")?.TextContent;
-                string value = element.QuerySelector("td")?.TextContent;
+                string key = element.QuerySelector("dt")?.TextContent;
+                string value = element.QuerySelector("dd")?.TextContent;
                 Console.WriteLine($"Ключ: {key}");
                 Console.WriteLine($"Значение: {value}");
                 Console.WriteLine(new string('-', 30));
                 result.Add(key, value);
             }
 
-            var fullDescription = doc.Body.QuerySelector(".option__text").TextContent;
+            var fullDescription = doc.Body.QuerySelector("div[data-widget='webDescription']").TextContent;
             result.Add("Описание", fullDescription);
 
-            var images = doc.QuerySelectorAll("li img");
+            var images = doc.QuerySelectorAll("div[data-widget='webGallery'] img");
             foreach (var image in images)
             {
-                Console.WriteLine(image.GetAttribute("src").Replace("c246x328", "big"));
+                var picSrc = image.GetAttribute("src");
+                if (picSrc.Contains("video"))
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(image.GetAttribute("src").Replace("wc50", "wc1000"));
+                }
+                
             }
 
             return result;
