1e054d147b8ead0b303e785d2770d563967d81a2
Соколовский Илья Игоревич <sokolguitarman@gmail.com>
2024-03-27 22:23:01 +0300

    Optimized code, ready wb parser

--- HTML test/Solution1/BaltikaParser/Program.cs
+++ HTML test/Solution1/BaltikaParser/Program.cs
@@ -1,54 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using AngleSharp;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using SeleniumUndetectedChromeDriver;
-
-namespace BaltikaParsers
+﻿using System.Threading.Tasks;
+namespace Parser
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
-            var driver = Setup();
-            Thread.Sleep(5000);
-            driver.FindElement(By.XPath(
-                 @"//button[@class='product-page__btn-detail j-wba-card-item j-wba-card-item-show j-wba-card-item-observe']")).Click();
-            Thread.Sleep(5000);
-            var html = driver.PageSource;
-            var config = Configuration.Default;
-            var context = BrowsingContext.New(config);
-            var doc = await context.OpenAsync(req => req.Content(html));
-            var descriptionElements = doc.Body.QuerySelectorAll("div.product-details .product-params__row");
-            foreach (var element in descriptionElements)
-            {
-                string key = element.QuerySelector("th")?.TextContent;
-                string value = element.QuerySelector("td")?.TextContent;
-                Console.WriteLine($"Ключ: {key}");
-                Console.WriteLine($"Значение: {value}");
-                Console.WriteLine(new string('-',30));
-            }
+            var parser = new WebParser();
+            var dict = await parser.ParseWb("https://www.wildberries.ru/catalog/24825388/detail.aspx");
         }
-        public static UndetectedChromeDriver Setup()
-        {
-            var options = new ChromeOptions();
-            options.AddArguments(new List<string>()
-            {
-            });
-            var driver = UndetectedChromeDriver.Create(options,
-                driverExecutablePath: @"C:\Users\ISokolovskiy\Desktop\Angle Sharp test\HTML test\Solution1\HtmlTester\chromedriver.exe");
-            driver.Manage().Window.Maximize();
-            return driver;
-
-            // var options = new ChromeOptions();
-            // options.AddArguments("--incognito");
-            // var driver = new ChromeDriver(options);
-            // driver.Navigate().GoToUrl("https://www.okeydostavka.ru/msk/pivo-baltika-0-bezalkogol-noe-0-5-0-45l-zh-b");
-        }
-        driver.GoToUrl("https://www.wildberries.ru/catalog/24825388/detail.aspx");
-        
     }
 }
---
+++ HTML test/Solution1/BaltikaParser/WebParser.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AngleSharp;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumUndetectedChromeDriver;
+namespace Parser
+{
+ internal class WebParser
+    {
+        private UndetectedChromeDriver _driver;
+        public WebParser()
+        {
+            Setup();
+        }
+        public void Setup()
+        {
+            var options = new ChromeOptions();
+            //options.AddArguments("headless");                                     не работает
+            _driver = UndetectedChromeDriver.Create(options,
+                driverExecutablePath:
+                @"chromedriver.exe");
+            _driver.Manage().Window.Maximize();
+        }
+        public async Task<Dictionary<string, string>> ParseWb(string url)
+        {
+            _driver.GoToUrl(url);
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            await Task.Delay(5000);
+
+            _driver.FindElement(By.XPath("//button[@class='product-page__btn-detail j-wba-card-item j-wba-card-item-show j-wba-card-item-observe']")).Click();
+            await Task.Delay(5000);
+
+            var html = _driver.PageSource;
+            var config = Configuration.Default;
+            var context = BrowsingContext.New(config);
+            var doc = await context.OpenAsync(req => req.Content(html));
+
+            var descriptionElements = doc.Body.QuerySelectorAll("div.product-details .product-params__row");
+            foreach (var element in descriptionElements)
+            {
+                string key = element.QuerySelector("th")?.TextContent;
+                string value = element.QuerySelector("td")?.TextContent;
+                Console.WriteLine($"Ключ: {key}");
+                Console.WriteLine($"Значение: {value}");
+                Console.WriteLine(new string('-', 30));
+                result.Add(key, value);
+            }
+
+            var fullDescription = doc.Body.QuerySelector(".option__text").TextContent;
+            result.Add("Описание", fullDescription);
+
+            var images = doc.QuerySelectorAll("li img");
+            foreach (var image in images)
+            {
+                Console.WriteLine(image.GetAttribute("src").Replace("c246x328", "big"));
+            }
+
+            return result;
+        }
+        
+        
+        public async Task<Dictionary<string, string>> ParseOzon(string url)
+        {
+            _driver.GoToUrl(url);
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            await Task.Delay(5000);
+
+            _driver.FindElement(By.XPath("//button[@class='product-page__btn-detail j-wba-card-item j-wba-card-item-show j-wba-card-item-observe']")).Click();
+            await Task.Delay(5000);
+
+            var html = _driver.PageSource;
+            var config = Configuration.Default;
+            var context = BrowsingContext.New(config);
+            var doc = await context.OpenAsync(req => req.Content(html));
+
+            var descriptionElements = doc.Body.QuerySelectorAll("div.product-details .product-params__row");
+            foreach (var element in descriptionElements)
+            {
+                string key = element.QuerySelector("th")?.TextContent;
+                string value = element.QuerySelector("td")?.TextContent;
+                Console.WriteLine($"Ключ: {key}");
+                Console.WriteLine($"Значение: {value}");
+                Console.WriteLine(new string('-', 30));
+                result.Add(key, value);
+            }
+
+            var fullDescription = doc.Body.QuerySelector(".option__text").TextContent;
+            result.Add("Описание", fullDescription);
+
+            var images = doc.QuerySelectorAll("li img");
+            foreach (var image in images)
+            {
+                Console.WriteLine(image.GetAttribute("src").Replace("c246x328", "big"));
+            }
+
+            return result;
+        }
+    }
+}
